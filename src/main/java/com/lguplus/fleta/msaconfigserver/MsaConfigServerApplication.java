107123d9c4c33846e094a3e74c08c99b4f3d0d15
MZ01-THONG <thong@mz.co.kr>
2022-06-15 09:06:59 +0700

    add config server

---
+++ src/main/java/com/lguplus/fleta/msaconfigserver/MsaConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.lguplus.fleta.msaconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class MsaConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaConfigServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lguplus/fleta/msaconfigserver/MsaConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lguplus.fleta.msaconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
